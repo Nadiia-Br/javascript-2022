5fdba180cd1347b2343a19bdbc175f3e19e8473c
Nadiia-Br <nadiia.brn@gmail.com>
2022-03-03 15:41:15 +0200

    add hw8

--- lesson8/hw8/js_hw8.js
+++ lesson8/hw8/js_hw8.js
@@ -4,9 +4,9 @@
 //     - Напишіть код,  котрий :
 // -- отримує текст з параграфа з id "content"
 
-// let elementById = document.getElementById('content');
-// console.log(elementById);
-// console.log(elementById.innerText);
+let elementById = document.getElementById('content');
+console.log(elementById);
+console.log(elementById.innerText);
 
 
 // -- отримує текст з блоку з id "rules"
@@ -27,9 +27,26 @@ let changeRules = document.getElementById('rules');
 console.log(changeRules.innerText = 'ПРАВИЛА');
 
 // -- змініть кожному елементу колір фону на червоний
-
-let children = document.body.children;
-
 // -- змініть кожному елементу колір тексту на синій
+
+// let children = document.body.children;
+// for (const child of children) {
+//     child.style.background = 'red';
+//     child.style.color = 'blue';
+// }
+
+
 // -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+// console.log(document.getElementById('rules').classList);
+
+// let cl = document.getElementById('rules');
+// console.log(cl.classList);
+
+
 // -- поміняти колір тексту у всіх елементів fc_rules на червоний
+
+let fc_rules = document.getElementsByClassName('fc_rules');
+for (const rule of fc_rules) {
+    rule.style.background ='red';
+}
