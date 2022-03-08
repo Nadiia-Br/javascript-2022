f72da874adcb5cc9d16ac9da0d56bff2d5314c86
Nadiia-Br <nadiia.brn@gmail.com>
2022-03-08 03:49:52 +0200

    add hw8

---
+++ lesson9/hw9/js_hw9.js
@@ -0,0 +1,53 @@
+// Все робити за допомоги js.
+// - створити блок,
+//     - додати йому класи wrap, collapse, alpha, beta
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// - додати цей блок в body.
+// - клонувати його повністю, та додати клон в body.
+
+let elDiv = document.createElement('div');
+elDiv.innerText='okten';
+elDiv.classList.add('wrap', 'collapse', 'alpha', 'beta');
+elDiv.style.background='lightgreen';
+elDiv.style.color='orange';
+elDiv.style.fontSize='150px';
+document.body.appendChild(elDiv);
+
+let cloneDiv = elDiv.cloneNode(true);
+document.body.appendChild(cloneDiv);
+
+
+
+// - Є масив:
+//     ['Main','Products','About us','Contacts']
+// Взяти файл template1.html та додати в нього скріпт котрий для кожного елементу масиву створює li та додає його до блоку .menu
+// Завдання робити через цикли.
+
+let arr
+
+
+
+
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// Завдання робити через цикли.
+//
+// - Є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+//     Завдання робити через цикли.
