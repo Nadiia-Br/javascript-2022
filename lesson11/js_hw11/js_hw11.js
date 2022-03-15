621b6c437314e7e31772bdf0d898ca2bf89df7f1
Nadiia-Br <nadiia.brn@gmail.com>
2022-03-15 09:52:51 +0200

    add hw11

---
+++ lesson11/js_hw11/js_hw11.js
@@ -0,0 +1,35 @@
+// -створити форму з інпутами для name та age.
+//     При відправці форми записати об'єкт в localstorage
+
+let info = document.forms.first;
+first.onsubmit = function (e) {
+    e.preventDefault();
+
+    let yourName = first.name.value;
+    let yourAge = first.age.value;
+    let user = {yourName, yourAge};
+    localStorage.setItem('data', JSON.stringify(user));
+
+};
+
+
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
+let arr = JSON.parse(localStorage.getItem('cars')) || [];
+
+let infoCar = document.forms.car;
+car.onsubmit = function (ev) {
+    ev.preventDefault();
+
+
+    let model = car.model.value;
+    let type = car.type.value;
+    let volume = car.volume.value;
+
+    let allInfo = {model, type, volume};
+
+    arr.push (allInfo);
+    localStorage.setItem('allInfo', JSON.stringify((arr)));
+
+}
