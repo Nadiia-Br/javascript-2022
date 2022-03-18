c049fbfe2e9f6f60ac6df0b13861839ead1299dc
Nadiia-Br <nadiia.brn@gmail.com>
2022-03-18 15:05:33 +0200

    add hw12

--- lesson12/hw12/js_hw12.js
+++ lesson12/hw12/js_hw12.js
@@ -5,35 +5,41 @@
 // https://jsonplaceholder.typicode.com/posts
 //
 
-let wrap = document.getElementById('wrap');
+// let wrap = document.getElementById('wrap');
+//
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(value => value.json())
+//     .then(value => {
+//         console.log(value);
+//         for (let valueElement of value) {
+//             let postEl = document.createElement('div');
+//             postEl.classList.add('post');
+//
+//             let divuserid = document.createElement('div');
+//             let divid = document.createElement('div');
+//             let divtitle = document.createElement('div');
+//             let divbody = document.createElement('div');
+//
+//             divuserid.innerText = `user ID = ${valueElement.userId}` ;
+//             divid.innerText = `ID -  ${valueElement.id}` ;
+//             divtitle.innerText = valueElement.title;
+//             divbody.innerText = valueElement.body;
+//
+//             divuserid.classList.add('userid');
+//             divid.classList.add('id');
+//             divtitle.classList.add('title');
+//             divbody.classList.add('body');
+//
+// postEl.append(divid, divuserid, divtitle, divbody);
+//             wrap.appendChild(postEl);
+//         }
+//     });
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(value => value.json())
-    .then(value => {
-        console.log(value);
-        for (let valueElement of value) {
-            let postEl = document.createElement('div');
-            postEl.classList.add('post');
 
-            let divuserid = document.createElement('div');
-            let divid = document.createElement('div');
-            let divtitle = document.createElement('div');
-            let divbody = document.createElement('div');
 
-            divuserid.innerText = `user ID = ${valueElement.userId}` ;
-            divid.innerText = `ID -  ${valueElement.id}` ;
-            divtitle.innerText = valueElement.title;
-            divbody.innerText = valueElement.body;
 
-            divuserid.classList.add('userid');
-            divid.classList.add('id');
-            divtitle.classList.add('title');
-            divbody.classList.add('body');
 
-postEl.append(divid, divuserid, divtitle, divbody);
-            wrap.appendChild(postEl);
-        }
-    });
+
 
 
 //     2.
@@ -42,3 +48,40 @@ postEl.append(divid, divuserid, divtitle, divbody);
 // Всі характеристики повинні мати свої блоки всередені div.comment
 // https://jsonplaceholder.typicode.com/comments
 
+let com = document.getElementsByClassName('comments')[0];
+
+fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(value => value.json())
+    .then(value => {
+        console.log(value);
+
+        for (let coment of value) {
+            let divcom = document.createElement('div');
+            divcom.classList.add('comment');
+
+            let divpostid = document.createElement('div');
+            let divid = document.createElement('div');
+            let divname = document.createElement('div');
+            let divbody = document.createElement('div');
+            let divemail = document.createElement('div');
+
+            divpostid.innerText = `post ID = ${coment.postId}` ;
+            divid.innerText = `ID -  ${coment.id}` ;
+            divname.innerText = coment.name;
+            divbody.innerText = coment.body;
+            divemail.innerText = coment.email;
+
+            divpostid.classList.add('postid');
+            divid.classList.add('id2');
+            divname.classList.add('name2');
+            divbody.classList.add('body2');
+            divemail.classList.add('email');
+
+
+            com.appendChild(divcom);
+            divcom.append(divpostid, divid, divname, divbody, divemail);
+
+
+
+        }
+    });
