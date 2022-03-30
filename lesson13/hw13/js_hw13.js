bad6c3fca87d2b93f3ea1116ddfeda91f2a8439f
Nadiia-Br <nadiia.brn@gmail.com>
2022-03-31 01:23:13 +0300

    add hw13

--- lesson13/hw13/js_hw13.js
+++ lesson13/hw13/js_hw13.js
@@ -1,32 +1,101 @@
+// console.log('My daily routine');
+// setTimeout(() => {
+//     let counter = 1;
+//     console.log('I wake up', counter);
+//     setTimeout(() => {
+//         counter++;
+//         console.log('I brush my teeth', counter);
+//         setTimeout(() => {
+//             counter++;
+//             console.log('I have breakfast', counter);
+//             setTimeout(() => {
+//                 counter++;
+//                 console.log('I get dressed', counter);
+//                 setTimeout(() => {
+//                     counter++;
+//                     console.log('I work', counter);
+//                     setTimeout(() => {
+//                         counter++;
+//                         console.log('I read a book', counter);
+//                         setTimeout(() => {
+//                             counter++;
+//                             console.log('I go to bed', counter);
+//                         }, 1000)
+//                     }, 1000)
+//                 }, 1000)
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 2000);
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
 console.log('My daily routine');
-setTimeout(() => {
+new Promise((resolve, reject) => {
     let counter = 1;
-    console.log('I wake up', counter);
     setTimeout(() => {
+        console.log('I wake up', counter);
         counter++;
-        console.log('I brush my teeth', counter);
-        setTimeout(() => {
-            counter++;
-            console.log('I have breakfast', counter);
+        resolve(counter);
+
+    }, 1000);
+})
+    .then((counter) => {
+
+        return new Promise((resolve, reject) => {
             setTimeout(() => {
+                console.log('I brush my teeth', counter);
                 counter++;
+                resolve(counter);
+            }, 2000)
+        })
+    })
+    .then(counter => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('I have breakfast', counter);
+                counter++;
+                resolve(counter);
+
+            }, 3000)
+        });
+    })
+    .then(counter => {
+       return new Promise((resolve, reject) => {
+            setTimeout(() => {
                 console.log('I get dressed', counter);
-                setTimeout(() => {
-                    counter++;
-                    console.log('I  work', counter);
-                    setTimeout(() => {
-                        counter++;
-                        console.log('I read a book', counter);
-                        setTimeout(() => {
-                            counter++;
-                            console.log('I go to bed', counter);
-                        }, 1000)
-                    }, 1000)
-                }, 1000)
-            }, 1000)
-        }, 1000)
-    }, 1000)
-}, 2000);
+                counter++;
+                resolve(counter)
+            }, 2000);
+        });
+    })
+    .then(counter => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('I work', counter);
+                counter++;
+                resolve(counter);
 
-
-
+            }, 3000)
+        });
+    })
+    .then(counter => {
+       return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('I read a book', counter);
+                counter++;
+                resolve(counter)
+            }, 2000);
+        });
+    })
+    .then(counter => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log('I go to bed', counter);
+                counter++;
+                resolve(counter)
+            }, 2000);
+        });
+    })
